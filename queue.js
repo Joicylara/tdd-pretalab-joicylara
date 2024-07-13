146262582ad32dfef157d48c7f807ec07b901b27
joicy kelly <joicykelly2301@gmail.com>
2024-07-13 16:49:29 -0300

    feat:add metodos e testes do desafio

--- queue.js
+++ queue.js
@@ -4,7 +4,30 @@ class Queue {
     this.elements = []
   }
   
-  // Implementar aqui
+  size(){
+    return this.elements.length;
+  }
+
+  add(item){
+    return this.elements.push(item);
+  }
+  
+  peek(){
+    return this.elements[0];
+  }
+
+  dequeue(){
+    return this.elements.pop();
+  }
+
 }
 
 module.exports = Queue
+
+
+/*
+size() Deve ser capaz de calcular o tamanho da fila
+add(item) Deve ser capaz de adicionar um item na fila
+peek() Deve escolher o primeiro item da fila
+dequeue() Deve remover o primeiro item da fila
+*/
--- queue.test.js
+++ queue.test.js
@@ -3,23 +3,47 @@ const Queue = require('./queue')
 describe('Queue', () => {
   it('Deve criar uma fila', () => {
     const queue = new Queue()
-
     expect(queue).toEqual({ elements: []})
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+    queue.add('JavaScript');
+    queue.add('Java');
+    queue.add('Python');
+    queue.add('C++');
+    expect(queue.size()).toEqual(4);
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    const queue = new Queue();
+    expect(queue.add('JavaScript')).toEqual(1);
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+    queue.add('JavaScript');
+    queue.add('Java');
+    queue.add('Python');
+    queue.add('C++');
+    expect(queue.peek()).toEqual('JavaScript');
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+    queue.add('JavaScript');
+    queue.add('Java');
+    queue.add('Python');
+    queue.add('C++');
+    expect(queue.dequeue()).toEqual('C++');
   })
 })
+
+
+
+/*
+size() Deve ser capaz de calcular o tamanho da fila
+add(item) Deve ser capaz de adicionar um item na fila
+peek() Deve escolher o primeiro item da fila
+dequeue() Deve remover o primeiro item da fila
+*/
